250a300b0d616f043b8d9b8301fef0470c36704c
tjc29 <tjc29@genpc38.medschl.cam.ac.uk>
2016-07-11 09:34:33 +0100

    use test data

--- bws/tests/test_bws.py
+++ bws/tests/test_bws.py
@@ -1,13 +1,18 @@
+import os
+
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APIRequestFactory, APIClient
-from rest_framework.authentication import TokenAuthentication
+
+from boadicea import settings
 
 
 class BwsTests(TestCase):
+    TEST_DATA_DIR = os.path.join(settings.PROJECT_DIR, 'tests', 'data')
 
     def setUp(self):
         ''' Create a user and set up the test client. '''
@@ -27,7 +32,7 @@ class BwsTests(TestCase):
     def test_token_auth_bws(self):
         ''' Test POSTing to the BWS using token authentication. '''
         url = reverse('bws')
-        pedigree_data = open("/home/MINTS/tjc29/bwa4_beta_pedigree_data_B.txt", "r")
+        pedigree_data = open(os.path.join(BwsTests.TEST_DATA_DIR, "pedigree_data.txt"), "r")
         data = {'mut_freq': 'UK', 'cancer_rates': 'UK',
                 'pedigree_data': pedigree_data}
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
@@ -37,7 +42,7 @@ class BwsTests(TestCase):
     def test_force_auth_bws(self):
         ''' Test POSTing to the BWS bypassing authentication. '''
         url = reverse('bws')
-        pedigree_data = open("/home/MINTS/tjc29/bwa4_beta_pedigree_data_B.txt", "r")
+        pedigree_data = open(os.path.join(BwsTests.TEST_DATA_DIR, "pedigree_data.txt"), "r")
         data = {'mut_freq': 'UK', 'cancer_rates': 'UK',
                 'pedigree_data': pedigree_data}
         self.client.force_authenticate(user=self.user)
