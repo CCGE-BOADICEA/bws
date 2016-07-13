0e9aa7805e20414910abfdaf778e6430a70d4184
tjc29 <tjc29@genpc38.medschl.cam.ac.uk>
2016-07-13 09:30:28 +0100

    test missing fields

--- bws/tests/test_bws.py
+++ bws/tests/test_bws.py
@@ -7,6 +7,8 @@ from django.test import TestCase
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APIRequestFactory, APIClient
+from django.utils.encoding import force_text
+import json
 
 
 class BwsTests(TestCase):
@@ -29,8 +31,13 @@ class BwsTests(TestCase):
         data = {'mut_freq': 'UK', 'cancer_rates': 'UK',
                 'pedigree_data': pedigree_data}
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
-        response = self.client.post(url, data, format='multipart')
+        response = self.client.post(url, data, format='multipart',
+                                    HTTP_ACCEPT="application/json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        content = json.loads(force_text(response.content))
+        self.assertTrue("mutation_frequency" in content)
+        self.assertTrue("pedigree_result" in content)
+        self.assertTrue("family_id" in content["pedigree_result"][0])
 
     def test_force_auth_bws(self):
         ''' Test POSTing to the BWS bypassing authentication. '''
@@ -41,3 +48,15 @@ class BwsTests(TestCase):
         self.client.force_authenticate(user=self.user)
         response = self.client.post(url, data, format='multipart')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_missing_fields(self):
+        ''' Test POSTing with missing fields. '''
+        url = reverse('bws')
+        data = {'mut_freq': 'UK'}
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        response = self.client.post(url, data, format='multipart',
+                                    HTTP_ACCEPT="application/json")
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        content = json.loads(force_text(response.content))
+        self.assertEqual(content['cancer_rates'][0], 'This field is required.')
+        self.assertEqual(content['pedigree_data'][0], 'This field is required.')
