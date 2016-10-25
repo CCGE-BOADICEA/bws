c43951d605e58de21c370b6a467b3eb0f1561d0a
tcarver <you@example.com>
2016-10-25 11:57:42 +0100

    add logging mixin to throttle classes

--- bws/rest_api.py
+++ bws/rest_api.py
@@ -25,17 +25,33 @@ from rest_framework.compat import is_authenticated
 logger = logging.getLogger(__name__)
 
 
-class BurstRateThrottle(UserRateThrottle):
+class LogThrottleMixin():
+    """ Add logging for occasions when throttle stops request. """
+
+    def allow_request(self, request, view):
+        """ Implement the check to see if the request should be throttled. """
+        success = super(LogThrottleMixin, self).allow_request(request, view)
+        if not success:
+            return self.throttle_fail(request.user)
+        return success
+
+    def throttle_fail(self, user):
+        """ Called when a request to the API has failed due to throttling. """
+        logger.warning("Request throttled (" + self.__class__.__name__ + "); USER: "+str(user))
+        return False
+
+
+class BurstRateThrottle(LogThrottleMixin, UserRateThrottle):
     """ Throttle short burst of requests from a user. """
     scope = 'burst'
 
 
-class SustainedRateThrottle(UserRateThrottle):
+class SustainedRateThrottle(LogThrottleMixin, UserRateThrottle):
     """ Throttle sustained requests from a user. """
     scope = 'sustained'
 
 
-class EndUserIDRateThrottle(SimpleRateThrottle):
+class EndUserIDRateThrottle(LogThrottleMixin, SimpleRateThrottle):
     """
     Limits the rate of API calls that may be made by a given end user.
     The end user id plus the user will be used as a unique cache key.
