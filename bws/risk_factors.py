c6c8ef5de18d3a61850727a1bfe3e35f26c46bff
tcarver <tjc29@cam.ac.uk>
2017-01-27 11:21:46 +0000

    add risk factors web service

--- bws/risk_factors.py
+++ bws/risk_factors.py
@@ -1,11 +1,21 @@
 from boadicea.exceptions import RiskFactorError
 from collections import OrderedDict
+from rest_framework.views import APIView
+from rest_framework_xml.renderers import XMLRenderer
+from rest_framework.renderers import BrowsableAPIRenderer, JSONRenderer
+from bws.throttles import BurstRateThrottle, EndUserIDRateThrottle,\
+    SustainedRateThrottle
+from rest_framework import serializers, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import BasicAuthentication,\
+    SessionAuthentication, TokenAuthentication
 
 
 class RiskFactors(object):
-    ''' Risk factors are defined in terms of a categories. An unknown risk factory
-    is category 0, otherwise they are given a non-zero number depending on which
-    group they belong to. '''
+    ''' Each risk factor for an individual is defined in terms of a category they are in.
+        For an unknown risk factor it is category 0, otherwise they are given a non-zero number
+        depending on which group they belong to. '''
 
     categories = OrderedDict([
         ('menarche_age', 7),                # <11, 11, 12, 13, 14, 15, >15
@@ -58,12 +68,7 @@ class RiskFactors(object):
         # Define the number of categories for each factor
         n_categories = list(RiskFactors.categories.values())
         n_factors = len(n_categories)
-
-        # Calcaulate the maximum allowed risk factor code.
-        max_factor = 1
-        for i in n_categories:
-            max_factor *= i + 1
-        max_factor -= 1
+        max_factor = RiskFactors.get_max_factor()
 
         # Read in the risk factor code and convert it to integer
         if not isinstance(factor, int):
@@ -83,3 +88,89 @@ class RiskFactors(object):
             category.append(int(dividend % (n_categories[i] + 1)))
             dividend = (dividend - category[-1]) / (n_categories[i] + 1)
         return category
+
+    @staticmethod
+    def get_max_factor():
+        ''' Calcaulate the maximum allowed risk factor code. '''
+        n_categories = list(RiskFactors.categories.values())
+        max_factor = 1
+        for i in n_categories:
+            max_factor *= i + 1
+        max_factor -= 1
+        return max_factor
+
+
+class RiskFactorsInputSerializer(serializers.Serializer):
+    ''' Risk Factor Categories '''
+    for key, value in RiskFactors.categories.items():
+        exec(key.lower() + " = serializers.IntegerField(required=False, "
+             "max_value="+str(value)+", min_value=0, default=0)")
+
+
+class RiskFactorsOutputSerializer(serializers.Serializer):
+    """ Boadicea result. """
+    factor = serializers.IntegerField(max_value=RiskFactors.get_max_factor())
+
+
+class RiskFactorsView(APIView):
+    renderer_classes = (XMLRenderer, JSONRenderer, BrowsableAPIRenderer, )
+    serializer_class = RiskFactorsInputSerializer
+    authentication_classes = (SessionAuthentication, BasicAuthentication, TokenAuthentication, )
+    permission_classes = (IsAuthenticated,)
+    throttle_classes = (BurstRateThrottle, SustainedRateThrottle, EndUserIDRateThrottle)
+
+    def post(self, request):
+        """
+        Each risk factor for an individual is defined in terms of a category they are in.
+        For an unknown risk factor it is category 0, otherwise they are given a non-zero number
+        depending on which group they belong to.
+        ---
+        parameters_strategy: merge
+        response_serializer: RiskFactorsOutputSerializer
+        parameters:
+           - name: menarche_age
+             description: age of menarche, categories are <11, 11, 12, 13, 14, 15, >15
+             type: integer
+           - name: parity
+             description: categories are Nulliparous, 1 birth, 2 births, >2 births
+             type: integer
+           - name: age_of_first_live_birth
+             description: categories are <20, 20-24, 25-29, >29
+             type: integer
+           - name: oral_contraception
+             description: categories are never, former, current
+             type: integer
+           - name: mht
+             description: categories are never, former, current e-type, current c-type
+             type: integer
+           - name: bmi
+             description: categories are <18.5, 18.5-24.9, 25-29.9, >=30
+             type: integer
+           - name: alcohol_intake
+             description: categories are 0g, <5g, 5-14g, 15-24g, 25-34g, 35-44g, >=45g
+             type: integer
+           - name: age_of_menopause
+             description: categories are <40, 40-44, 45-49, 50-54, >54
+             type: integer
+           - name: mammographic_density
+             description: categories are BI-RADS 1, 2, 3, 4
+             type: integer
+
+        responseMessages:
+           - code: 401
+             message: Not authenticated
+
+        consumes:
+           - application/json
+           - application/xml
+        produces: ['application/json', 'application/xml']
+        """
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            validated_data = serializer.validated_data
+            category = []
+            for key in RiskFactors.categories.keys():
+                category.append(validated_data.get(key))
+            factor_serializer = RiskFactorsOutputSerializer({'factor': RiskFactors.encode(category)})
+            return Response(factor_serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
