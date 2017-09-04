7644da74adff499bcbf78529ee3233e311fc55a0
tcarver <tjc29@cam.ac.uk>
2017-09-04 09:23:59 +0100

    categories made from list of risk factors, add space_name() snake_name()

--- bws/risk_factors.py
+++ bws/risk_factors.py
@@ -25,6 +25,16 @@ class RiskFactor(object):
         ''' Convert camel to snake case e.g. AgeOfFirstLiveBirth -> age_of_first_live_birth '''
         return re.sub(r'((?<=[a-z])[A-Z]|(?<!\A)[A-Z](?=[a-z]))', r'_\1', label).lower()
 
+    @classmethod
+    def snake_name(cls):
+        ''' Return the class name in snake format (e.g. menarche_age) '''
+        return cls.camel_to_snake(cls.__name__)
+
+    @classmethod
+    def space_name(cls):
+        ''' Return the class name in space separated format (e.g. Menarche Age) '''
+        return cls.camel_to_space(cls.__name__)
+
 
 class MenarcheAge(RiskFactor):
     cats = ['-', '<11', '11', '12', '13', '14', '15', '>15']
@@ -84,6 +94,7 @@ class RiskFactors(object):
         depending on which group they belong to. These are then combined into a single
         risk factor code (see encode() function) that is used by the BOADICEA risk calculation. '''
 
+    # list of risk factor classes
     risk_factors = [
         MenarcheAge,
         Parity,
@@ -97,19 +108,24 @@ class RiskFactors(object):
         Height
     ]
 
-    categories = OrderedDict([
-        ('menarche_age', 7),                # <11, 11, 12, 13, 14, 15, >15
-        ('parity', 4),                      # Nulliparous, 1 birth, 2 births, >2 births
-        ('age_of_first_live_birth', 4),     # <20, 20-24, 25-29, >29
-        ('oral_contraception', 3),          # never, former, current
-        ('mht', 4),                         # menopause hormone replacement;
-                                            # never, former, current e-type, current c-type
-        ('bmi', 4),                         # <18.5, 18.5-24.9, 25-29.9, >=30
-        ('alcohol_intake', 7),              # 0g, <5g, 5-14g, 15-24g, 25-34g, 35-44g, >=45g
-        ('age_of_menopause', 5),            # <40, 40-44, 45-49, 50-54, >54
-        ('mammographic_density', 4),        # Breast Imaging Reporting and Data System; BI-RADS 1, 2, 3, 4
-        ('height', 5)                       # Height/cm <150.17, 150.17-158.26, 158.26-165.82, 165.82-173.91, >173.91
-        ])
+    # dictionary of risk factor name and number of categories
+    categories = OrderedDict(
+        (rf.snake_name(), len(rf.cats)-1) for rf in risk_factors
+    )
+
+#     categories = OrderedDict([
+#         ('menarche_age', 7),                # <11, 11, 12, 13, 14, 15, >15
+#         ('parity', 4),                      # Nulliparous, 1 birth, 2 births, >2 births
+#         ('age_of_first_live_birth', 4),     # <20, 20-24, 25-29, >29
+#         ('oral_contraception', 3),          # never, former, current
+#         ('mht', 4),                         # menopause hormone replacement;
+#                                             # never, former, current e-type, current c-type
+#         ('bmi', 4),                         # <18.5, 18.5-24.9, 25-29.9, >=30
+#         ('alcohol_intake', 7),              # 0g, <5g, 5-14g, 15-24g, 25-34g, 35-44g, >=45g
+#         ('age_of_menopause', 5),            # <40, 40-44, 45-49, 50-54, >54
+#         ('mammographic_density', 4),        # Breast Imaging Reporting and Data System; BI-RADS 1, 2, 3, 4
+#         ('height', 5)                       # Height/cm <150.17, 150.17-158.26, 158.26-165.82, 165.82-173.91, >173.91
+#         ])
 
     @staticmethod
     def encode(risk_categories):
