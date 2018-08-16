579de32a811ef904022c23fd01c838821d1b51be
tcarver <tjc29@cam.ac.uk>
2018-08-16 12:03:47 +0100

    add tests for ows

---
+++ bws/tests/test_ows.py
@@ -0,0 +1,88 @@
+""" Ovarian web-service testing.  """
+
+from boadicea_auth.models import UserDetails
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+from django.utils.encoding import force_text
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APIRequestFactory, APIClient
+import json
+import os
+from django.test.utils import override_settings
+
+
+class OwsTests(TestCase):
+    TEST_BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+    TEST_DATA_DIR = os.path.join(TEST_BASE_DIR, 'tests', 'data')
+
+    def setUp(self):
+        ''' Create a user and set up the test client. '''
+        self.factory = APIRequestFactory()
+        self.client = APIClient(enforce_csrf_checks=True)
+        self.user = User.objects.create_user('testuser', email='testuser@test.com',
+                                             password='testing')
+        # add user details
+        UserDetails.objects.create(user=self.user, job_title=UserDetails.CGEN,
+                                   country='UK')
+        self.user.save()
+        self.token = Token.objects.create(user=self.user)
+        self.token.save()
+        self.url = reverse('ows')
+        self.pedigree_data = open(os.path.join(OwsTests.TEST_DATA_DIR, "canrisk_data_v1.txt"), "r")
+
+    def test_token_auth_ows(self):
+        ''' Test POSTing to the OWS using token authentication. '''
+        data = {'mut_freq': 'UK', 'cancer_rates': 'UK',
+                'pedigree_data': self.pedigree_data,
+                'user_id': 'test_XXX'}
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        response = self.client.post(self.url, data, format='multipart', HTTP_ACCEPT="application/json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        content = json.loads(force_text(response.content))
+        self.assertTrue("mutation_frequency" in content)
+        self.assertTrue("pedigree_result" in content)
+        self.assertTrue("family_id" in content["pedigree_result"][0])
+
+    def test_multi_pedigree_ows(self):
+        ''' Test POSTing multiple pedigrees to the OWS. '''
+        multi_pedigree_data = open(os.path.join(OwsTests.TEST_DATA_DIR, "multi_canrisk_data_v1.txt"), "r")
+        data = {'mut_freq': 'UK', 'cancer_rates': 'UK',
+                'pedigree_data': multi_pedigree_data,
+                'user_id': 'test_XXX'}
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        response = self.client.post(self.url, data, format='multipart',
+                                    HTTP_ACCEPT="application/json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        content = json.loads(force_text(response.content))
+        self.assertEqual(len(content['pedigree_result']), 2, "two results")
+        family_ids = ["XXX0", "XXX1"]
+        for res in content['pedigree_result']:
+            self.assertTrue(res['family_id'] in family_ids)
+
+    def test_ows_warnings(self):
+        ''' Test warning when proband has already had ovarian cancer and no risks are reported. '''
+        # change proband to have had OC
+        pd = self.pedigree_data.read().replace('1967\t0\t0\t0', '1967\t0\t0\t51')
+        data = {'mut_freq': 'UK', 'cancer_rates': 'UK',
+                'pedigree_data': pd, 'user_id': 'test_XXX'}
+        self.client.force_authenticate(user=self.user)
+        response = self.client.post(self.url, data, format='multipart',
+                                    HTTP_ACCEPT="application/json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        content = json.loads(force_text(response.content))
+        self.assertTrue('cancer_risks not provided' in content['warnings'])
+
+    @override_settings(FORTRAN_TIMEOUT=0.01)
+    def test_ows_timeout(self):
+        ''' Test a timeout error is reported by the web-service. '''
+        data = {'mut_freq': 'UK', 'cancer_rates': 'UK',
+                'pedigree_data': self.pedigree_data, 'user_id': 'test_XXX'}
+        self.client.force_authenticate(user=self.user)
+        response = self.client.post(self.url, data, format='multipart',
+                                    HTTP_ACCEPT="application/json")
+        self.assertEqual(response.status_code, status.HTTP_408_REQUEST_TIMEOUT)
+        content = json.loads(force_text(response.content))
+        self.assertTrue('detail' in content)
+        self.assertTrue('Request has timed out.' in content['detail'])
