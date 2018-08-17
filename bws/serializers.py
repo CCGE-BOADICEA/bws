588d5fd49525b3697673bdb8951fa2f9810f3132
tcarver <tjc29@cam.ac.uk>
2018-08-17 10:07:13 +0100

    update docs

--- bws/serializers.py
+++ bws/serializers.py
@@ -107,6 +107,7 @@ class OwsExtendedInputSerializer(OwsInputSerializer):
 
 
 class PedigreeResultSerializer(serializers.Serializer):
+    """ Cancer model risks and mutation probabilities for a pedigree. """
     family_id = serializers.CharField(read_only=True)
     proband_id = serializers.CharField(read_only=True)
     cancer_risks = serializers.ListField(read_only=True, required=False)
@@ -119,7 +120,7 @@ class PedigreeResultSerializer(serializers.Serializer):
 
 
 class OutputSerializer(serializers.Serializer):
-    """ Boadicea result. """
+    """ Cancer model results. """
     version = serializers.CharField(read_only=True)
     timestamp = serializers.DateTimeField(read_only=True)
     mutation_frequency = serializers.DictField(read_only=True)
