f9a5fc19bd905bac68c18fb7f5840b1cb56448e8
tcarver <tjc29@cam.ac.uk>
2019-05-20 08:58:02 +0100

    fix test_age_yob

--- bws/tests/tests_pedigree_validation.py
+++ bws/tests/tests_pedigree_validation.py
@@ -217,7 +217,7 @@ class PersonTests(TestCase, ErrorTests):
         pedigree_file = deepcopy(self.pedigree_file)
         m2 = pedigree_file.pedigrees[0].get_person_by_name('M2')
         m2.age = "0"
-        m2.cancers.diagnoses.prc.age = "0"
+        m2.cancers.diagnoses.prc.age = "-1"
         warnings = PedigreeFile.validate(pedigree_file.pedigrees)
         self.assertRegex(warnings[0], "year of birth and age at last follow up must be specified")
 
