de22a968ed0f9fd9bce8056958e5970689032ff5
tcarver <tjc29@cam.ac.uk>
2019-06-10 14:48:06 +0100

    code restructure to enable files within a directory to be processed

--- bws/scripts/run_webservice.py
+++ bws/scripts/run_webservice.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 #
-# Run a risk prediction via the web-service.
+# Run risk predictions via the web-service.
 #
 # optional arguments:
 #  -h, --help            show this help message and exit
 #  --mut_freq {UK,Ashkenazi,Iceland,Custom}
 #                        Mutation Frequencies (default: UK)
-#  --cancer_rates {UK,Australia,Canada,USA-white,Denmark,Finland,Iceland,New-Zealand,Norway,Spain,Sweden}
+#  --cancer_rates {UK,Australia,Canada,USA,Denmark,Finland,Iceland,New-Zealand,Norway,Spain,Sweden}
 #                        Cancer incidence rates (default: UK)
 #  --url URL             Web-services URL
 #  -u USER, --user USER  Username
@@ -51,6 +51,74 @@ import requests
 import json
 import argparse
 import csv
+import os
+from os import listdir
+from os.path import join, isfile
+
+
+def output_tab(tabf, cmodel, rjson, bwa):
+    ''' Tab delimeted output file '''
+    if cmodel == "boadicea":
+        header = ["FamID", "IndivID", "Age", "BCRisk          ", "BCRisk%    ",
+                  "OCRisk          ", "OCRisk%"]
+    else:
+        header = ["FamID", "IndivID", "Age", "OCRisk          ", "OCRisk%"]
+    with open(tabf, 'a') as csvfile:
+        writer = csv.writer(csvfile, delimiter='\t')
+        writer.writerow(["pedigree", bwa])
+        writer.writerow(["version", rjson["version"]])
+        writer.writerow(["timestamp", rjson["timestamp"]])
+        writer.writerow(["cancer incidence rates", rjson["cancer_incidence_rates"]])
+        writer.writerow(["note: baseline cancer risks are provided in brackets"])
+        results = rjson["pedigree_result"]
+        for res in results:
+            famid = res["family_id"]
+            indivID = res["proband_id"]
+
+            if "cancer_risks" in res:
+                cancer_risks = res["cancer_risks"]
+                bcancer_risks = res["baseline_cancer_risks"]
+
+                writer.writerow(header)
+                for idx, cr in enumerate(cancer_risks):
+                    bcr = bcancer_risks[idx]
+                    if bcr["age"] == cr["age"]:
+                        if cmodel == "boadicea":
+                            bc_dec = '{} ({})'.format(cr["breast cancer risk"]["decimal"],
+                                                      bcr["breast cancer risk"]["decimal"])
+                            bc_per = '{} ({})'.format(cr["breast cancer risk"]["percent"],
+                                                      bcr["breast cancer risk"]["percent"])
+                        oc_dec = '{} ({})'.format(cr["ovarian cancer risk"]["decimal"],
+                                                  bcr["ovarian cancer risk"]["decimal"])
+                        oc_per = '{} ({})'.format(cr["ovarian cancer risk"]["percent"],
+                                                  bcr["ovarian cancer risk"]["percent"])
+                        if cmodel == "boadicea":
+                            writer.writerow([famid, indivID, cr["age"], bc_dec, bc_per, oc_dec, oc_per])
+                        else:
+                            writer.writerow([famid, indivID, cr["age"], oc_dec, oc_per])
+            writer.writerow([])
+    csvfile.close()
+
+
+def runws(args, data, bwa, cancers):
+    ''' Call web-services '''
+    data["mut_freq"] = args.mut_freq
+    data["cancer_rates"] = args.cancer_rates
+
+    for cmodel in cancers:
+        files = {'pedigree_data': open(bwa, 'rb')}
+        r = requests.post(url+cmodel+'/', data=data, files=files, auth=(user, pwd))
+        if r.status_code == 200:
+            rjson = r.json()
+            if args.tab:
+                output_tab(args.tab, cmodel, rjson, bwa)
+            else:
+                print(json.dumps(rjson, indent=4, sort_keys=True))
+        else:
+            print("Error status: "+str(r.status_code))
+            print(r.json())
+            exit(1)
+
 
 #
 # define optional command line arguments
@@ -73,13 +141,13 @@ for gene in genes:
     group2.add_argument('--'+gene+'_mut_sensitivity', type=float, help=gene+' mutation sensitivity')
 
 parser.add_argument('--cancer_rates', default='UK',
-                    choices=['UK', 'Australia', 'Canada', 'USA-white', 'Denmark', 'Finland',
+                    choices=['UK', 'Australia', 'Canada', 'USA', 'Denmark', 'Finland',
                              'Iceland', 'New-Zealand', 'Norway', 'Spain', 'Sweden'],
                     help='Cancer incidence rates (default: %(default)s)')
 
 parser.add_argument('--url', default='https://canrisk.org/', help='Web-services URL')
 parser.add_argument('-u', '--user', help='Username')
-parser.add_argument('-p', '--ped', help='CanRisk (or BOADICEA v4) pedigree file')
+parser.add_argument('-p', '--ped', help='CanRisk (or BOADICEA v4) pedigree file or directory of pedigree file(s)')
 parser.add_argument('-t', '--tab', help='Tab delimeted output file name')
 
 args = parser.parse_args()
@@ -124,63 +192,15 @@ else:
     print("Error status: "+str(r.status_code))
     exit(1)
 
-# 2. run BOADICEA risk prediction for a given pedigree
-bwa = input("Pedigree (BOADICEA v4 file): ") if args.ped is None else args.ped
+# 2. run risk prediction for a given pedigree or all files in a directory
+bwa = input("Pedigree (BOADICEA v4/CanRisk file) or path to directory of pedigrees: ") if args.ped is None else args.ped
 
-data["mut_freq"] = args.mut_freq
-data["cancer_rates"] = args.cancer_rates
+# if bwa is a directory then get files in directory
+if os.path.isdir(bwa):
+    bwas = [join(bwa, f) for f in listdir(bwa) if isfile(join(bwa, f))]
+else:
+    bwas = [bwa]
 
-# open output file
-if args.tab:
-    open(args.tab, 'w')
-
-for cmodel in cancers:
-    files = {'pedigree_data': open(bwa, 'rb')}
-    r = requests.post(url+cmodel+'/', data=data, files=files, auth=(user, pwd))
-    if r.status_code == 200:
-        rjson = r.json()
-
-        # tab delimeted output file
-        if args.tab:
-            if cmodel == "boadicea":
-                header = ["FamID", "IndivID", "Age", "BCRisk          ", "BCRisk%    ", "OCRisk          ", "OCRisk%"]
-            else:
-                header = ["FamID", "IndivID", "Age", "OCRisk          ", "OCRisk%"]
-            with open(args.tab, 'a') as csvfile:
-                writer = csv.writer(csvfile, delimiter='\t')
-                writer.writerow(["version", rjson["version"]])
-                writer.writerow(["timestamp", rjson["timestamp"]])
-                writer.writerow(["cancer incidence rates", rjson["cancer_incidence_rates"]])
-                writer.writerow(["note: baseline cancer risks are provided in brackets"])
-                results = rjson["pedigree_result"]
-                for res in results:
-                    famid = res["family_id"]
-                    indivID = res["proband_id"]
-                    cancer_risks = res["cancer_risks"]
-                    bcancer_risks = res["baseline_cancer_risks"]
-
-                    writer.writerow(header)
-                    for idx, cr in enumerate(cancer_risks):
-                        bcr = bcancer_risks[idx]
-                        if bcr["age"] == cr["age"]:
-                            if cmodel == "boadicea":
-                                bc_dec = '{} ({})'.format(cr["breast cancer risk"]["decimal"],
-                                                          bcr["breast cancer risk"]["decimal"])
-                                bc_per = '{} ({})'.format(cr["breast cancer risk"]["percent"],
-                                                          bcr["breast cancer risk"]["percent"])
-                            oc_dec = '{} ({})'.format(cr["ovarian cancer risk"]["decimal"],
-                                                      bcr["ovarian cancer risk"]["decimal"])
-                            oc_per = '{} ({})'.format(cr["ovarian cancer risk"]["percent"],
-                                                      bcr["ovarian cancer risk"]["percent"])
-                            if cmodel == "boadicea":
-                                writer.writerow([famid, indivID, cr["age"], bc_dec, bc_per, oc_dec, oc_per])
-                            else:
-                                writer.writerow([famid, indivID, cr["age"], oc_dec, oc_per])
-                    writer.writerow([])
-            csvfile.close()
-        else:
-            print(json.dumps(rjson, indent=4, sort_keys=True))
-    else:
-        print("Error status: "+str(r.status_code))
-        print(r.json())
-        exit(1)
+for bwa in bwas:
+    print(bwa)
+    runws(args, data, bwa, cancers)
