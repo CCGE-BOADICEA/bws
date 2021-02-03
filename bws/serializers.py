c6a8b33b9f76125f679caa55f57a54d9dfd65446
tcarver <tjc29@medschl.cam.ac.uk>
2021-02-03 15:09:24 +0000

    update max length of tenyr_ages

--- bws/serializers.py
+++ bws/serializers.py
@@ -101,7 +101,7 @@ class BwsExtendedInputSerializer(BwsInputSerializer):
 
 
 class BCTenYrSerializer(BwsExtendedInputSerializer):
-    tenyr_ages = serializers.CharField(validators=[int_list_validator], min_length=3, max_length=10)
+    tenyr_ages = serializers.CharField(validators=[int_list_validator], min_length=3, max_length=30)
 
 
 class OwsExtendedInputSerializer(OwsInputSerializer):
