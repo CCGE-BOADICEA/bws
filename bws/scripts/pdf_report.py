4dc1af1862c79feb5d6352451422af7173a55972
tcarver <tjc29@cam.ac.uk>
2021-07-30 13:17:41 +0100

    update pdf_report

--- bws/scripts/pdf_report.py
+++ bws/scripts/pdf_report.py
@@ -45,15 +45,23 @@ class Thread_With_Trace(threading.Thread):
         self.killed = True
 
 
-def wait_for_statfile(cls, fname="canrisk_report.pdf", max_time=10, time_interval=0.25):
+def rm_report(fname="canrisk_report.pdf"):
     ''' Wait for file to exist on download. '''
     home = expanduser("~")
     fname = join(home, 'Downloads', fname)
     if os.path.isfile(fname):
         os.remove(fname)
+
+
+def wait_for_statfile(fname="canrisk_report.pdf", max_time=10, time_interval=0.25, rename=None):
+    ''' Wait for file to exist on download. '''
+    home = expanduser("~")
+    fname = join(home, 'Downloads', fname)
     for _i in range(int(max_time/time_interval)):
         if os.path.isfile(fname) and os.stat(fname).st_size != 0:
             print(str(_i*time_interval)+"s wait for file save")
+            if rename:
+                os.rename(fname, rename)
             return
         time.sleep(time_interval)
     print(fname+" not saved")
@@ -67,9 +75,12 @@ def run_server():
         httpd.serve_forever()
 
 
-def get_html(url, token, ows_result, bws_result):
+def create_pdf(url, token, ows_result, bws_result, bwa, pdf_name):
+    cwd = os.getcwd()
     base = join(DIRECTORY, 'base.html')
+    pedigree = join(DIRECTORY, 'pedigree.txt')
     copyfile(join(os.path.dirname(os.path.realpath(__file__)), 'base.html'), base)
+    copyfile(bwa, pedigree)
     data = {"ows_result": json.dumps(ows_result.json(), separators=(',', ':')),
             "bws_result": json.dumps(bws_result.json(), separators=(',', ':'))}
     r = requests.post(url+'combine/', data=data, headers={'Authorization': "Token "+token})
@@ -84,7 +95,8 @@ def get_html(url, token, ows_result, bws_result):
 
     t = Thread_With_Trace(target=run_server)
     t.start()
+    rm_report()
     webbrowser.open('http://0.0.0.0:8081/base.html', new=2)
-    wait_for_statfile("/home/tim/Downloads/canrisk_report.pdf")
+    wait_for_statfile(rename=join(cwd, pdf_name))
     os.remove(base)
     t.kill()
