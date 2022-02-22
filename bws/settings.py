02309258c641a442e2bf72bbe997af1b4e9459c2
tcarver <tjc29@cam.ac.uk>
2022-02-22 14:39:53 +0000

    add EMERGE

--- bws/settings.py
+++ bws/settings.py
@@ -136,6 +136,7 @@ BC_MODEL = {
     'PRS_REFERENCE_FILES': OrderedDict([
         ('BCAC 313', 'BCAC_313_PRS.prs'),
         ('BRIDGES 306', 'BRIDGES_306_PRS.prs'),
+        ('EMERGE 309', 'EMERGE_309_PRS.prs'),
         ('PERSPECTIVE 295', 'PERSPECTIVE_295_PRS.prs'),
         ('PRISMA 289', 'PRISM_289_PRS.prs')
     ])
