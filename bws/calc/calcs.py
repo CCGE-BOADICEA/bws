8565fb76a3c5a4c0becfacc4d1c84702888f6e43
tim <tim@Ubuntu22.04.4.myguest.virtualbox.org>
2024-04-09 12:07:32 +0100

    use ethnicity for prostate cancer

--- bws/calc/calcs.py
+++ bws/calc/calcs.py
@@ -330,8 +330,7 @@ class Predictions():
         mname = str(model.get('NAME', ""))
         if param_file is not None:
             cmd.extend(["-s", param_file])
-        if mname != "PC":
-            cmd.extend(["-e", os.path.join(model["HOME"], 'Data', "coeffs-"+mname+"_"+model_params.ethnicity.get_filename())])
+        cmd.extend(["-e", os.path.join(model["HOME"], 'Data', "coeffs-"+mname+"_"+model_params.ethnicity.get_filename())])
 
         out = model_opts.out
         cmd.extend(model_opts.get_cmd_line_opts())
