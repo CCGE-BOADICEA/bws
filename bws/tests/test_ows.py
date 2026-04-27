037137fae506944a5fd80664fdcb1e2f63b3e3db
Tim Carver <tjc29@cam/.ac.uk>
2026-04-27 13:24:20 +0000

    pytest update

--- bws/tests/test_ows.py
+++ bws/tests/test_ows.py
@@ -6,6 +6,11 @@ SPDX-FileCopyrightText: 2023 University of Cambridge
 SPDX-License-Identifier: GPL-3.0-or-later
 """
 
+import json
+import os
+import pytest
+
+from bws.pedigree_file import PedigreeFile
 from django.contrib.auth.models import User, Permission
 from django.test import TestCase
 from django.test.utils import override_settings
@@ -14,9 +19,6 @@ from django.utils.encoding import force_str
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APIClient
-import json
-import os
-from bws.pedigree_file import PedigreeFile
 
 
 class OwsTests(TestCase):
@@ -46,6 +48,7 @@ class OwsTests(TestCase):
         TestCase.tearDown(self)
         self.pedigree_data.close()
 
+    @pytest.mark.req_ws_160
     def test_ows_output(self):
         ''' Test output of POSTing to the OWS using token authentication. '''
         data = {'mut_freq': 'UK', 'cancer_rates': 'UK',
@@ -67,6 +70,7 @@ class OwsTests(TestCase):
         self.assertGreater(len(pedigree_result["cancer_risks"]), 0)
         self.assertTrue("family_id" in pedigree_result)
 
+    @pytest.mark.req_ws_161
     def test_multi_pedigree_ows(self):
         ''' Test POSTing multiple pedigrees to the OWS. '''
         multi_pedigree_data = open(os.path.join(OwsTests.TEST_DATA_DIR, "multi", "d2.canrisk"), "r")
@@ -102,6 +106,7 @@ class OwsTests(TestCase):
             self.assertTrue("baseline_cancer_risks" in res)
             self.assertTrue("cancer_risks" in res)
 
+    @pytest.mark.req_ws_017
     def test_ows_bwa(self):
         ''' Test web-service takes BWA file as input. '''
         pedigree_data = open(os.path.join(OwsTests.TEST_DATA_DIR, "d3.bwa"), "r")
@@ -113,6 +118,7 @@ class OwsTests(TestCase):
         pedigree_data.close()
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+    @pytest.mark.req_ws_162
     def test_ows_warnings(self):
         ''' Test warning when proband has already had ovarian cancer and no risks are reported. '''
         # change proband to have had OC
@@ -125,6 +131,7 @@ class OwsTests(TestCase):
         content = json.loads(force_str(response.content))
         self.assertTrue('cancer_risks not provided' in content['warnings'])
 
+    @pytest.mark.req_ws_041
     @override_settings(FORTRAN_TIMEOUT=0.001)
     def test_ows_timeout(self):
         ''' Test a timeout error is reported by the web-service. '''
@@ -162,6 +169,7 @@ class OwsTestsPRS(TestCase):
     def tearDown(self):
         self.pedigree_data.close()
 
+    @pytest.mark.req_ws_163
     def test_prs_in_canrisk_file(self):
         '''
         Test ovarian cancer PRS parameters defined in the header of CanRisk formatted file.
